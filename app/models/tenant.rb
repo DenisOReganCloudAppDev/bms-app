4c629e8302d2106c2d9d9033b97d112a4afdb609
DenisRegan <denismartinoregan@hotmail.com>
2023-08-10 17:24:31 +0100

    updated app

--- app/models/tenant.rb
+++ app/models/tenant.rb
@@ -1,5 +1,6 @@
 class Tenant < ApplicationRecord
 	belongs_to :building
+	has_many :maintenance_requests, dependent: :destroy
 	validates:first_name, presence:true
 	validates:last_name, presence:true
 	validates:email, presence:true
